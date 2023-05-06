432e5a673b93da2c5f1742a5aef5086c549e7ac8
Michael Aloh <mykelaloh@gmail.com>
2023-05-06 01:43:49 -0700

    done

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+"""distributes archives to web servers"""
+
+
+from fabric.api import *
+import os.path
+
+env.hosts = ['54.173.84.235', '34.207.155.43']
+
+
+def do_deploy(archive_path):
+    """ distributes an archive to your web servers"""
+    if not os.path.exists(archive_path):
+        return False
+
+    try:
+        path = archive_path
+        file_name = path.split('/')[-1]
+        put(path, '/tmp/{}'.format(file_name))
+
+        print(file_name, file_name.split('.')[0])
+        run('mkdir -p /data/web_static/releases/{}'.format(file_name.split('.')[0]))
+        run('tar -xzf /tmp/{} -C /data/web_static/releases/{}/'
+            .format(file_name, file_name.split('.')[0]))
+        run('rm -rf /data/web_static/releases/{}/web_static'.format(
+            file_name.split('.')[0]))
+        run('rm -rf /data/web_static/current')
+        # Create new symbolic link /data/web_static/current
+        run('ln -s /data/web_static/releases/{}/ \
+            /data/web_static/current'.format(file_name.split('.')[0]))
+        return True
+
+    except Exception as e:
+        print(e)
+        return False
